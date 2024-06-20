14b408254bc156f8765da84a183fc406f5559ee4
Nishant <vikaskumar.e1256@gmail.com>
2024-06-20 18:46:31 +0530

    Error Handling

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -28,10 +30,13 @@ app.use('/api/v1/users', userRouter);
 // If route does not exist
 app.all('*', (req, res, next) =>
 {
-    res.status(404).json({
-        status: 'fail',
-        message: "Can't find " + req.originalUrl + " on this server"
-    })
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: "Can't find " + req.originalUrl + " on this server"
+    // })
 })
+
+app.use(globalErrorHandler);
 
 module.exports = app;
